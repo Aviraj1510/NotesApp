77df29d7274f92ea6eda31a1492cd4a22c09e9a5
Aviraj1510 <panchotiyaaviraj@gmail.com>
2024-06-06 19:02:25 +0530

    StickyNotesApp

--- app/src/main/java/com/example/notesapp/AppWidget.java
+++ app/src/main/java/com/example/notesapp/AppWidget.java
@@ -14,27 +14,22 @@ import android.widget.RemoteViews;
 public class AppWidget extends AppWidgetProvider {
 
 
-    // Create an Intent to launch activity
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
 
-        // Perform this loop procedure for each App Widget that belongs to this
-        // provider
+
         for (int appWidgetId : appWidgetIds) {
-            // Create an Intent to launch MainActivity
+
             Intent launchActivity = new Intent(context, MainActivity.class);
-            // Attaching a Pending Intent to trigger it when application is not alive
+
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchActivity, PendingIntent.FLAG_IMMUTABLE);
 
-            // Get the layout for the App Widget and attach an on-click listener
-            // to the button
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
             remoteViews.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
 
 
-            // Tell the AppWidgetManager to perform an update on the current app
-            // widget
             appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
         }
     }
